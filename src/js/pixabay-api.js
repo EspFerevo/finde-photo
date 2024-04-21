ed4b5d9c751fb91b67617ef0e8dca79c7b6b9f8b
EspFerevo <olekchekrenov@gmail.com>
2024-04-21 21:16:46 +0300

    Axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,8 @@
 'use strict';
 
-export async function searchImages(query) {
+export async function searchImages(query, page) {
   const apiKey = '43322409-71cdbf6ef8f62fb1f69ac5609';
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`;
 
   const response = await fetch(apiUrl);
 
--- src/main.js
+++ src/main.js
@@ -1,39 +1,80 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+'use strict';
 
+import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
 import { searchImages } from './js/pixabay-api.js';
 import { displayImages } from './js/render-functions.js';
 
-document
-  .getElementById('search-form')
-  .addEventListener('submit', async function (event) {
-    event.preventDefault();
-    const query = document.getElementById('search-input').value.trim();
-    if (query === '') {
-      iziToast.error({
-        title: 'Error',
-        message: 'Please enter a search keyword.',
+const loadBtn = document.querySelector('.load-btn');
+const input = document.querySelector('#search-input');
+
+let page = 1;
+let searchWord = null;
+
+document.getElementById('search-form').addEventListener('submit', handleSubmit);
+loadBtn.addEventListener('click', onBtnClick);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  page = 1;
+  const gallery = document.getElementById('gallery');
+  gallery.innerHTML = '';
+  const loader = document.getElementById('loader');
+  loader.style.display = 'block';
+  searchWord = input.value.trim();
+
+  try {
+    const data = await searchImages(searchWord);
+    if (data.hits.length === 0) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+    displayImages(data.hits);
+    if (data.total > 15) {
+      loadBtn.style.display = 'block';
+    }
+  } catch (error) {
+    iziToast.error({ title: 'Error', message: error.message });
+  } finally {
+    loader.style.display = 'none';
+    input.value = '';
+  }
+}
+
+async function onBtnClick() {
+  page += 1;
+  const loader = document.getElementById('loader');
+  loader.style.display = 'block';
+  try {
+    const data = await searchImages(searchWord, page);
+    displayImages(data.hits);
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    const lastPage = Math.ceil(data.totalHits / 15);
+    if (lastPage === page) {
+      loadBtn.style.display = 'none';
+      iziToast.show({
+        message: `We're sorry, but you've reached the end of search results.`,
+        messageColor: '#fff',
+        position: 'bottomRight',
+        backgroundColor: '#cb73fc',
+        progressBar: false,
+        close: false,
+        timeout: 5000,
       });
-      return;
     }
-
-    const gallery = document.getElementById('gallery');
-    gallery.innerHTML = '';
-    document.getElementById('search-input').value = '';
-    const loader = document.getElementById('loader');
-    loader.style.display = 'block';
-
-    try {
-      const data = await searchImages(query);
-      if (data.hits.length === 0) {
-        throw new Error(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-      }
-      displayImages(data.hits);
-    } catch (error) {
-      iziToast.error({ title: 'Error', message: error.message });
-    } finally {
-      loader.style.display = 'none';
-    }
-  });
+  } catch (error) {
+    iziToast.error({ title: 'Error', message: error.message });
+  } finally {
+    loader.style.display = 'none';
+  }
+}
